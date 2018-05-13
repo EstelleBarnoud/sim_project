d4bb09f09c6b4ac9a931f7a3038195bee66ac939
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2018-05-13 15:49:49 +0200

    component Enonce added

---
+++ idees-citoyennes/src/Enonce.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Enonce extends React.Component
+{
+    render(){
+        return(
+                <div>
+                    <h2>Enoncé</h2>
+                    <p>Décriver votre projet en quelques mots</p>
+                </div>
+              );
+    }
+}
+
+export default Enonce;
