a631e1aa088f363f4a945e681a5e7f0c74b830b2
unknown <estelle.barnoud@telecom-parsitech.fr>
2018-05-20 20:57:56 +0200

    Home and project routed

--- idees-citoyennes/src/App.js
+++ idees-citoyennes/src/App.js
@@ -12,6 +12,7 @@ import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -20,34 +21,48 @@ const muiTheme = getMuiTheme({
   },
 });
 
+const Home = () => (
+  <div style={{padding: 40}}>
+    <h2>Home</h2>
+    <Link to="/project">Projet</Link>
+  </div>
+);
+
+const Project = () => (
+  <div className="App">
+    <Nav />
+    <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+      <Paper zDepth={2} >
+        <img src={ require('./image/ravi-roshan-383162-unsplash.jpg') } alt="" />
+        <div style={{padding: 50, paddingTop: 20}}>
+          <Title />
+          <Divider />
+          <Description />
+          <Divider />
+          <Progress />
+          <Divider />
+          <ReactionBar />
+          <Divider />
+          <Reactions />
+        </div>
+      </Paper>
+    </div>
+    <Links />
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
-          <header>
-            <MyAppBar />
-          </header>
-          <div className="App">
-            <Nav />
-            <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
-              <Paper zDepth={2} >
-                <img src={ require('./image/ravi-roshan-383162-unsplash.jpg') } alt="" />
-                <div style={{padding: 50, paddingTop: 20}}>
-                  <Title />
-                  <Divider />
-                  <Description />
-                  <Divider />
-                  <Progress />
-                  <Divider />
-                  <ReactionBar />
-                  <Divider />
-                  <Reactions />
-                </div>
-              </Paper>
-            </div>
-            <Links />
-          </div>
+          <Router>
+            <header>
+              <MyAppBar />
+              <Route exact path="/" component={Home} />
+              <Route path="/project" component={Project} />
+            </header>
+          </Router>
         </div>
       </ MuiThemeProvider>
     );
