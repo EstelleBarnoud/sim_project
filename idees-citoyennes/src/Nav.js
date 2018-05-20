210b5acbf5294dd8ebba86e8166c95a5cbd445c2
unknown <estelle.barnoud@telecom-parsitech.fr>
2018-05-20 13:10:05 +0200

    Nav style issue corrected

--- idees-citoyennes/src/Nav.js
+++ idees-citoyennes/src/Nav.js
@@ -7,14 +7,13 @@ import Filter_2 from 'material-ui/svg-icons/image/filter-2';
 import Filter_3 from 'material-ui/svg-icons/image/filter-3';
 import Filter_4 from 'material-ui/svg-icons/image/filter-4';
 import Filter_5 from 'material-ui/svg-icons/image/filter-5';
-import styled from "styled-components";
 
-const StyledListItem = styled(ListItem)`
-    padding-left: 70 !important;
-    padding-right: 70 !important;
-    text-align: "center" !important;
-    font-weight: "bold" !important;
-`;
+const style = {
+    paddingLeft: 50,
+    paddingRight: 50,
+    textAlign: "center",
+    fontWeight: "bold"
+  }  
 
 class Nav extends React.Component
 {
@@ -23,7 +22,7 @@ class Nav extends React.Component
                 <div>
                     <Paper zDepth={2} style={{marginLeft: -20, marginTop: -20}}>
                         <List>
-                            <StyledListItem primaryText="Top 5" />
+                            <ListItem primaryText="Top 5" style={style} />
                             <ListItem primaryText="Projet 1" leftIcon={<Filter_1 />} />
                             <ListItem primaryText="Projet 2" leftIcon={<Filter_2 />} />
                             <ListItem primaryText="Projet 3" leftIcon={<Filter_3 />} />
@@ -31,8 +30,8 @@ class Nav extends React.Component
                             <ListItem primaryText="Projet 5" leftIcon={<Filter_5 />} />
                         </List>
                         <Divider />
-                        <h3>Nouveau</h3>
                         <List>
+                            <ListItem primaryText="Nouveau" style={style} />
                             <ListItem primaryText="Projet 1" leftIcon={<Filter_1 />} />
                             <ListItem primaryText="Projet 2" leftIcon={<Filter_2 />} />
                             <ListItem primaryText="Projet 3" leftIcon={<Filter_3 />} />
@@ -40,8 +39,8 @@ class Nav extends React.Component
                             <ListItem primaryText="Projet 5" leftIcon={<Filter_5 />} />
                         </List>
                         <Divider />
-                        <h3>Les + inspirants</h3>
                         <List>
+                            <ListItem primaryText="Les + inspirants" style={style} />
                             <ListItem primaryText="Projet 1" leftIcon={<Filter_1 />} />
                             <ListItem primaryText="Projet 2" leftIcon={<Filter_2 />} />
                             <ListItem primaryText="Projet 3" leftIcon={<Filter_3 />} />
