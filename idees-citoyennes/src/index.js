89c5ba168cd907367b9be2cc01a05476aba5ad81
unknown <estelle.barnoud@telecom-parsitech.fr>
2018-05-23 14:01:36 +0200

    store and reducer created

--- idees-citoyennes/src/index.js
+++ idees-citoyennes/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = {}
+
+const reducer = combineReducers(reducers)
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
